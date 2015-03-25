52636f891d58ea4564d9a78702b9ebe0fa28ea62
Matt Jones <mattjones@yieldbot.com>
2015-03-25 13:32:18 -0400

    Add basic yardocs and file header

--- bin/handler-logstash.rb
+++ bin/handler-logstash.rb
@@ -1,28 +1,40 @@
-#!/usr/bin/env ruby
+#! /usr/bin/env ruby
 #
-# Sensu Logstash Handler
+#   handler-logstash
 #
-# Heavily inspried (er, copied from) the GELF Handler writeen by
-# Joe Miller.
+# DESCRIPTION:
+#   Designed to take sensu events, transform them into logstah JSON events
+#   and ship them to a redis server for logstash to index.  This also
+#   generates a tag with either 'sensu-ALERT' or 'sensu-RECOVERY' so that
+#   searching inside of logstash can be a little easier.
 #
-# Designed to take sensu events, transform them into logstash JSON events
-# and then either ship them to a redis server, or send them to a logstash
-# instance for logstash to index. This also generates a tag with either
-# 'sensu-ALERT' or 'sensu-RECOVERY' so that searching inside of logstash
-# can be a little easier.
+# OUTPUT:
+#   plain text
 #
-# Written by Zach Dunn -- @SillySophist or http://github.com/zadunn
+# PLATFORMS:
+#   Linux
 #
-# Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   gem: diplomat
+#
+# USAGE:
+#
+# NOTES:
+#   Heavily inspried (er, copied from) the GELF Handler writeen by
+#   Joe Miller.
+#
+# LICENSE:
+#   Zach Dunn @SillySophist http://github.com/zadunn
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
 
-require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-handler'
 require 'redis'
 require 'json'
 require 'socket'
 require 'time'
-require 'json'
 
 #
 # Logstash Handler
@@ -36,10 +48,10 @@ class LogstashHandler < Sensu::Handler
     @event['action'].eql?('resolve') ? 'RESOLVE' : 'ALERT'
   end
 
-  # rubocop:disable Metrics/AbcSize
   def handle
+    time = Time.now.utc.iso8601
     logstash_msg = {
-      :@timestamp    => Time.now.utc.iso8601,
+      :@timestamp    => time,
       :@version      => 1,
       :source        => ::Socket.gethostname,
       :tags          => ["sensu-#{action_to_string}"],
@@ -54,7 +66,6 @@ class LogstashHandler < Sensu::Handler
       :occurrences   => @event['occurrences'],
       :action        => @event['action']
     }
-
     logstash_msg[:type] = settings['logstash']['type'] if settings['logstash'].key?('type')
 
     case settings['logstash']['output']
