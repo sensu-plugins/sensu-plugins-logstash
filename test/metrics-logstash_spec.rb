e4ad6baca73dc3cd80843f79901bbf4dd2b33a9e
Mike Eves <meves23@gmail.com>
2017-11-04 18:31:51 +0000

    Add missing pipeline output duration metric (#23)
    * Add missing pipeline output duration metric
    
    * Fix Changelog formatting

--- test/metrics-logstash_spec.rb
+++ test/metrics-logstash_spec.rb
@@ -29,4 +29,22 @@ describe 'MetricsLogstash', '#run' do
       /node01.logstash.pipeline.plugins.inputs.4a3d87d316c088c052271a51fae0e37f47a193b9-30.events.queue_push_duration_in_millis 1162712/
     ).to_stdout.and raise_error(SystemExit)
   end
+
+  it 'returns output duration data' do
+    stub_request(:get, /_node\/stats/)
+      .with(basic_auth: %w(foo bar))
+      .to_return(
+        status: 200,
+        headers: {
+          'Content-Type' => 'application/json'
+        },
+        body: File.new('test/fixtures/node_stats.json')
+      )
+    args = %w(--user foo --password bar --host localhost --port 4200 --scheme node01.logstash)
+
+    check = LogstashNodeMetrics.new(args)
+    expect { check.run }.to output(
+      /node01.logstash.pipeline.plugins.outputs.4a3d87d316c088c052271a51fae0e37f47a193b9-33.events.duration_in_millis 24613/
+    ).to_stdout.and raise_error(SystemExit)
+  end
 end
